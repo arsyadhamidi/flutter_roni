602b51dc183e7d206fee41338182b52e49ea085b
Zulfiqar Tamin <fiqartamin@gmail.com>
2025-05-09 09:01:54 +0700

    fiqar

--- lib/ui/akun/akun_page.dart
+++ lib/ui/akun/akun_page.dart
@@ -108,10 +108,8 @@ class AkunPageView extends StatelessWidget {
                   SizedBox(height: 20),
                   InkWell(
                     onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => FaqPage()));
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => FaqPage()));
                     },
                     child: Padding(
                       padding: const EdgeInsets.only(left: 20, right: 20),
@@ -229,3 +227,5 @@ class AkunPageView extends StatelessWidget {
     );
   }
 }
+
+// test
